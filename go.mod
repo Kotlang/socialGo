c492e9b5fdba6db0c538d604f7b37147c80ae76d
Sai Nageswar S <sainageswar@gmail.com>
2021-11-16 12:49:36 +0530

    Adding API to upload post media directly to the service

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/Kotlang/socialGo
 go 1.16
 
 require (
-	github.com/SaiNageswarS/go-api-boot v0.1.9
+	github.com/SaiNageswarS/go-api-boot v0.1.13
 	github.com/aws/aws-sdk-go v1.34.28
 	github.com/google/uuid v1.2.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/jinzhu/copier v0.3.2
 	github.com/joho/godotenv v1.3.0
 	github.com/thoas/go-funk v0.8.0
-	go.mongodb.org/mongo-driver v1.5.3
+	go.mongodb.org/mongo-driver v1.7.2
 	go.uber.org/zap v1.17.0
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
--- service/feedpost_service.go
+++ service/feedpost_service.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/Kotlang/socialGo/db"
 	"github.com/Kotlang/socialGo/extensions"
@@ -9,6 +11,8 @@ import (
 	"github.com/Kotlang/socialGo/models"
 	s3client "github.com/Kotlang/socialGo/s3Client"
 	"github.com/SaiNageswarS/go-api-boot/auth"
+	"github.com/SaiNageswarS/go-api-boot/azure"
+	"github.com/SaiNageswarS/go-api-boot/bootUtils"
 	"github.com/SaiNageswarS/go-api-boot/logger"
 	"github.com/jinzhu/copier"
 	"github.com/thoas/go-funk"
@@ -129,4 +133,31 @@ func (s *FeedpostService) GetMediaUploadUrl(ctx context.Context, req *pb.MediaUp
 		UploadUrl: uploadUrl,
 		MediaUrl:  downloadUrl,
 	}, nil
+}
+
+func (s *FeedpostService) UploadPostMedia(stream pb.UserPost_UploadPostMediaServer) error {
+	userId, tenant := auth.GetUserIdAndTenant(stream.Context())
+	logger.Info("Uploading post media", zap.String("userId", userId), zap.String("tenant", tenant))
+	imageData, err := bootUtils.BufferGrpcServerStream(stream, func() ([]byte, error) {
+		req, err := stream.Recv()
+		if err != nil {
+			return nil, err
+		}
+		return req.ChunkData, nil
+	})
+	if err != nil {
+		logger.Error("Failed uploading image", zap.Error(err))
+		return err
+	}
+
+	// upload imageData to Azure bucket.
+	path := fmt.Sprintf("%s/%s/%d.jpg", tenant, userId, time.Now().Unix())
+	res := <-azure.Storage.UploadStream("social-posts", path, imageData)
+
+	if res.Err != nil {
+		logger.Error("Failed uploading media image.", zap.Error(res.Err))
+		return res.Err
+	}
+	stream.SendAndClose(&pb.UploadPostMediaResponse{UploadPath: res.Value.(string)})
+	return nil
 }
