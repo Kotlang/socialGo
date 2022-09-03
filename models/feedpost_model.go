c56e19eba25aa430b23d680ea44328e8ca97d5f6
SaiNageswarS <sainageswar@gmail.com>
2022-09-03 22:30:44 +0530

    Adding support for Mime Type in user post

--- models/feedpost_model.go
+++ models/feedpost_model.go
@@ -4,20 +4,25 @@ import (
 	"github.com/google/uuid"
 )
 
+type MediaUrl struct {
+	Url      string `bson:"url"`
+	MimeType string `bson:"mimeType"`
+}
+
 type FeedPostModel struct {
-	PostId        string   `bson:"_id"`
-	Title         string   `bson:"title"`
-	Post          string   `bson:"post"`
-	MediaUrls     []string `bson:"mediaUrls"`
-	ReferencePost string   `bson:"referencePost"`
-	Replies       []string `bson:"replies"`
-	PostType      string   `bson:"postType"`
-	UserId        string   `bson:"userId"`
-	NumLikes      int      `bson:"numLikes"`
-	NumShares     int      `bson:"numShares"`
-	NumReplies    int      `bson:"numReplies"`
-	Tags          []string `bson:"tags"`
-	CreatedOn     int64    `bson:"createdOn"`
+	PostId        string     `bson:"_id"`
+	Title         string     `bson:"title"`
+	Post          string     `bson:"post"`
+	MediaUrls     []MediaUrl `bson:"mediaUrls"`
+	ReferencePost string     `bson:"referencePost"`
+	Replies       []string   `bson:"replies"`
+	PostType      string     `bson:"postType"`
+	UserId        string     `bson:"userId"`
+	NumLikes      int        `bson:"numLikes"`
+	NumShares     int        `bson:"numShares"`
+	NumReplies    int        `bson:"numReplies"`
+	Tags          []string   `bson:"tags"`
+	CreatedOn     int64      `bson:"createdOn"`
 }
 
 func (m *FeedPostModel) Id() string {
