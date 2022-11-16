affd5f4f3083ae45a3fb59b97acee66257b3107b
Prince <this.is.kumar.prince@gmail.com>
2022-11-16 22:08:01 +0530

    Fix/getfeed returns webpreview (#6)
    * fix: returning webpreviews when user called GetFeed service
    
    * feat: social-model submodules updated

--- models/feedpost_model.go
+++ models/feedpost_model.go
@@ -9,21 +9,29 @@ type MediaUrl struct {
 	MimeType string `bson:"mimeType"`
 }
 
+type WebPreview struct {
+	Title        string `bson:"title"`
+	PreviewImage string `bson:"previewImage"`
+	Url          string `bson:"url"`
+	Description  string `bson:"description"`
+}
+
 type FeedPostModel struct {
-	PostId        string     `bson:"_id"`
-	Title         string     `bson:"title"`
-	Post          string     `bson:"post"`
-	MediaUrls     []MediaUrl `bson:"mediaUrls"`
-	ReferencePost string     `bson:"referencePost"`
-	Replies       []string   `bson:"replies"`
-	PostType      string     `bson:"postType"`
-	UserId        string     `bson:"userId"`
-	NumLikes      int        `bson:"numLikes"`
-	NumShares     int        `bson:"numShares"`
-	NumReplies    int        `bson:"numReplies"`
-	Tags          []string   `bson:"tags"`
-	CreatedOn     int64      `bson:"createdOn"`
-	Language      string     `bson:"language"`
+	PostId        string       `bson:"_id"`
+	Title         string       `bson:"title"`
+	Post          string       `bson:"post"`
+	MediaUrls     []MediaUrl   `bson:"mediaUrls"`
+	WebPreviews   []WebPreview `bson:"webPreviews"`
+	ReferencePost string       `bson:"referencePost"`
+	Replies       []string     `bson:"replies"`
+	PostType      string       `bson:"postType"`
+	UserId        string       `bson:"userId"`
+	NumLikes      int          `bson:"numLikes"`
+	NumShares     int          `bson:"numShares"`
+	NumReplies    int          `bson:"numReplies"`
+	Tags          []string     `bson:"tags"`
+	CreatedOn     int64        `bson:"createdOn"`
+	Language      string       `bson:"language"`
 }
 
 func (m *FeedPostModel) Id() string {
