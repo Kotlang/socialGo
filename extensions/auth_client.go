602b1150211a7ec9618fccbb52884f0e5098c556
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-11-22 23:55:31 +0530

    fix: grpc address

--- extensions/auth_client.go
+++ extensions/auth_client.go
@@ -25,7 +25,7 @@ func (c *AuthClient) getConnection() *grpc.ClientConn {
 	defer c.conn_creation_lock.Unlock()
 
 	if c.cached_conn == nil || c.cached_conn.GetState().String() != "READY" {
-		conn, err := grpc.Dial("20.193.225.77:50051", grpc.WithInsecure(), grpc.WithBlock())
+		conn, err := grpc.Dial("146.190.10.103:50051", grpc.WithInsecure(), grpc.WithBlock())
 		if err != nil {
 			logger.Error("Failed getting connection with auth service", zap.Error(err))
 			return nil
