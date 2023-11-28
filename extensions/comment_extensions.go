1e34f1a0dddd5e85b3cf62d2b9cbad9eb4f4eebd
Rajeev Singh <rsinghh68@gmail.com>
2023-11-28 14:48:10 +0530

    removed unnecesary comments

--- extensions/comment_extensions.go
+++ extensions/comment_extensions.go
@@ -13,15 +13,15 @@ func AttachCommentUserInfoAsync(
 	comment *socialPb.CommentProto,
 	userId, tenant, userType string) chan bool {
 
-	// logger.Info("AttachPostUserInfoAsync", zap.Any("feedPost", feedPost))
-
 	done := make(chan bool)
 
 	go func() {
 		comment.UserReactions = socialDb.React(tenant).GetUserReactions(comment.CommentId, userId)
+
 		// get comment author profile
 		authorProfile := <-GetSocialProfile(grpcContext, comment.UserId)
 		comment.AuthorInfo = authorProfile
+
 		done <- true
 	}()
 
